f44092d6478d8b28f7a156a590267d1abcb2dd54
TomasCumlin <78538798+TomasCumlin@users.noreply.github.com>
2023-03-21 10:51:43 +0100

    Update pangolin_linux_mac.py

--- src/pangolin_linux_mac.py
+++ src/pangolin_linux_mac.py
@@ -1,44 +1,119 @@
 #!/usr/bin/env python3
 
-import sys
+# This script runs the nextclade analysis on sequencing data. 
+# It can either be directily run in the terminal (command: python nc_terminal.py <sequence data> <batch name>)
+# or it can be used as a plugin in Geneious. 
+
+# Variables to be updates for each individual computer:
+    # 1. path (leave as an empty string if this will be used as a Geneious plugin)
+    # 2. output_path
+    # 3. nextclade_path
+
 import os
-import re
 import subprocess
+import sys
+import json
+import csv
+from datetime import date
+from os import listdir
+from os.path import isfile, join
 
-# Geneious input/output and options
-inFile = os.path.join("/geneious", sys.argv[2])
-outFile = os.path.join("/geneious", sys.argv[4])
+def terminal(var_name):
+    output, error= subprocess.Popen(var_name, universal_newlines=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    return output
 
-pathToDocker = sys.argv[6]
-pathToGeneiousData = sys.argv[8]
+def slash_correct(path, symbol):
+    correct_path = path.replace("\\", symbol)
+    return correct_path
 
-# Find path to temporary Geneious folder - folders named with largest numbers
-# Example: /Users/user/Geneious 2022.1 Data/transient/1660719270002/x/8/
-def getTaskPath(path):
-    sessionDirs = os.listdir(path)
-    sessions = [int(s) for s in sessionDirs if re.search('^([0-9]{13,13}){1}$', s)]
-    sessionsSort = sorted(sessions, reverse=True)
-    session = str(sessionsSort[0])
+# obtain today's date for the folder name
+today = str(date.today())
+today = today.replace("-", "_")
 
-    taskDirsPath = os.path.join(path, session, "x")
-    taskDirs = os.listdir(taskDirsPath)
-    tasks = [int(t) for t in taskDirs if re.search('^([0-9]{1,5}){1}$', t)]
-    tasksSort = sorted(tasks, reverse=True)
-    task = str(tasksSort[0])
 
-    taskPath = os.path.join(taskDirsPath, task, ":/geneious")
+infile=sys.argv[1]      # The selected sequences to analyse in Geneious
+batch_name=sys.argv[2]  # Name of the batch. Can be modified in Geneious.
 
-    return taskPath
+# If this is to be run in genious, variable "path" should be left as an empty string. If run in terminal, update it to where you want everything to be run.
+path=""
 
- 
-# Get latest pangolin Docker image
-subprocess.run( [pathToDocker, "pull", "covlineages/pangolin"] )
+# This is the path where all nextclade's output files should be saved.
+output_path=sys.argv[6]
+output_path=slash_correct(output_path, "/")
 
-# Run pangolin docker container: update pangolin + data and run fasta file from Geneious
+# Correct end by adding / if necessary 
+if (output_path[-1] != "/"):
+    output_path+="/"
 
-mountPath = getTaskPath(pathToGeneiousData)
-pangoCmd = ("pangolin --update ; pangolin " + str(inFile) + " --outfile " + str(outFile))
+nextclade_name= "nextclade_analysis_{}".format(today)
 
-subprocess.run( [pathToDocker, "run", "--rm" ,"-v", mountPath, \
-    "covlineages/pangolin", "/bin/bash", "-c", \
-    pangoCmd] ) # inFile = input all consensus fasta outFile = output pangolin .txt table
+# get a list of folders present in output_path
+onlyfiles = [f for f in listdir(output_path) if os.path.isdir(join(output_path, f))]
+
+counter = 1
+
+#if nextcalde_name already exists, the name is updated with a counter until it's unique. 
+while nextclade_name in onlyfiles:
+    nextclade_name="nextclade_analysis_{}_{}".format(today,str(counter))
+    counter +=1
+    onlyfiles = [f for f in listdir(output_path) if os.path.isdir(join(output_path, f))]
+
+# the path to where all files will end up
+output_permanent = output_path+nextclade_name
+
+# Path to where nextclade is installed.
+nextclade_path=sys.argv[4]
+nextclade_path=slash_correct(nextclade_path, "/")
+
+output=path+"output/"   #Path to where your results should be saved.
+
+# Path to where you want to store your downloaded database.
+# If you already have a database up-to-date, this step will be skipped.
+database_path=path+"data/sars-cov-2" 
+
+get_data=[nextclade_path, "dataset", "get", "--name=sars-cov-2", "--output-dir={}".format(database_path)]
+get_data_permanent=[nextclade_path, "dataset", "get", "--name=sars-cov-2", "--output-dir={}database/".format(output_permanent)]
+run_nextclade=[nextclade_path, "run", "--in-order", "--input-dataset", "{}".format(database_path), "--output-all={}".format(output), "--output-basename={}".format(batch_name), "{}".format(infile)]
+
+terminal(get_data)
+terminal(run_nextclade)
+#terminal(get_data_permanent)
+
+
+# Save software and database info
+
+#open the json-file
+f = open(database_path+"/tag.json")
+
+#convert the json-file to a dict
+data = json.load(f)
+
+#r.write(("Database version: {}").format(data['tag']))
+
+r = open("{}{}_final.csv".format(output, batch_name), 'w', newline='')
+writer = csv.writer(r)
+
+nextclade_vers = [nextclade_path, "--version"]
+
+output_results2  = terminal(nextclade_vers)
+
+with open('{}{}.csv'.format(output,batch_name), 'r') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=";")
+     for row in spamreader:
+         if "seqName" in row[0]:
+             row.append("Software version")
+             row.append("Database version")
+             writer.writerow(row)
+         else:
+             row.append(output_results2)
+             row.append(data['tag'])
+             writer.writerow(row)
+         
+f.close()
+r.close()
+
+# Copy all files from the temporary Geneious file to a new permanent file.
+copy_files=["cp", "-a", "{}.".format(output), "{}".format(output_permanent)]
+
+
+terminal(copy_files)
